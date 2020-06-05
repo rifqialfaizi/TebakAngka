2b91856446142a33a247c317b9cc8aa2ede35427
rifqialfaizi <rifqialfaizii@gmail.com>
2020-06-05 13:54:29 +0700

    first commit

---
+++ TebakAngka/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TebakAngka
+//
+//  Created by Rifqi Alfaizi on 05/06/20.
+//  Copyright © 2020 Rifqi Alfaizi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
