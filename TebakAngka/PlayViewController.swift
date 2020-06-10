a7b5c0838349cac19855e70c7da302850233e914
rifqialfaizi <rifqialfaizii@gmail.com>
2020-06-10 10:08:44 +0700

    Add Level

--- TebakAngka/PlayViewController.swift
+++ TebakAngka/PlayViewController.swift
@@ -13,16 +13,29 @@ class PlayViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var angkaYangDiTebak: UITextField!
     @IBOutlet weak var scoreTextLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var levelTextLabel: UILabel!
+    @IBOutlet weak var levelLabel: UILabel!
+    
     
     var score = 10
+    var level = 0
     
-    lazy var angkaRandomProperty: Int = generateRandomNumber()
+    
+    lazy var level1: Int = generateRandomNumber1()
+    lazy var level2: Int = generateRandomNumber2()
+    lazy var level3: Int = generateRandomNumber3()
+    lazy var level4: Int = generateRandomNumber4()
+    lazy var level5: Int = generateRandomNumber5()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set agar membuat nomor random saat masuk view saja, dan tidak me random angka di setiap tap button
-        _ = angkaRandomProperty
+        
+        _ = level1
+        
+//        level += 1
+//        levelLabel.text = String(level)
         
         // Hide keyboard when tapped outside keyboard
         self.angkaYangDiTebak.delegate = self
@@ -39,7 +52,8 @@ class PlayViewController: UIViewController, UITextFieldDelegate {
     @IBAction func tebakButtonDidTap(_ sender: Any) {
        
         // Angka yang harus di tebak
-        let angka = angkaRandomProperty
+        
+        var angka = level1
         print(angka)
         
         // Text Field
@@ -94,23 +108,64 @@ class PlayViewController: UIViewController, UITextFieldDelegate {
             showAlert2(title: "Waw kamu benar! \n Score kamu adalah \n \(score)" , message: "Hebat!")
             
             scoreLabel.text = String(score)
+            angkaYangDiTebak.text = ""
+
+            
+            // into the next level
+            
+            level += 1
+            levelLabel.text = String(level)
             
             // TODO: - ReGenerate Random Number
-
+            /*
             let alert = UIAlertController(title: "Bener jawabannya", message: "Mantul bgt cuy", preferredStyle: .alert)
             let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
                 self.navigationController?.popViewController(animated: true)
             }
             alert.addAction(okAction)
             self.present(alert, animated: true, completion: nil)
+            */
+            
+            // generate new random number
+            level1 = level2
+            print(angka)
+            level2 = level3
+            print(angka)
+            level3 = level4
+            print(angka)
+            level4 = level5
+            print(angka)
+            
+            // Menyimpan Score
+            let score1 = score
+            
+            // mengembalikan Score pada next level
+            score = 10
+            scoreLabel.text = String(score)
+            
             }
 }
     
-    func generateRandomNumber() -> Int {
+    func generateRandomNumber1() -> Int {
         let angkaRandom = Int.random(in: 1...10)
+        return angkaRandom
+    }
+    func generateRandomNumber2() -> Int {
+        let angkaRandom = Int.random(in: 11...20)
+        return angkaRandom
+    }
+    func generateRandomNumber3() -> Int {
+        let angkaRandom = Int.random(in: 21...30)
+        return angkaRandom
+    }
+    func generateRandomNumber4() -> Int {
+        let angkaRandom = Int.random(in: 31...40)
+        return angkaRandom
+    }
+    func generateRandomNumber5() -> Int {
+        let angkaRandom = Int.random(in: 41...50)
         return angkaRandom
     }
 }
 
-// cek 1 2 3
 
